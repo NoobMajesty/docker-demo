11f884ec4f6c380ee337a07556b78cd02f6c3a4f
NoobMajesty <47846708+NoobMajesty@users.noreply.github.com>
2022-10-31 22:14:51 +0200

    H

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hi matan');
+  res.send('hi amit and matan');
 });
 
 var server = app.listen(3000, function () {
